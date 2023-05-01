c1a72b6ad86395f70fc8255cdc0a4e13cfb4e912
Ricardo Peixoto <103431797+RcardoPs@users.noreply.github.com>
2023-05-01 15:43:34 -0300

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+
+  return (
+
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+
+    <Text> Agora Eu Sei React Native Papai </Text>
+
+    </View>
+    
+  );
+
+
+}
+
+export default HomePage;
